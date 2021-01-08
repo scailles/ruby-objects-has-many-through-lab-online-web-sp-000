cb8708f7b328188403506371eee843c1bb17f9c5
Shaina C <scailles23@gmail.com>
2021-01-08 04:19:05 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+  
+attr_accessor :name, :song, :genre 
+  
+@@all=[]
+  
+  
+def initialize(name)
+  @name
+  @songs=[]
+  @genres=[]
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+
+
+
+end
+  
