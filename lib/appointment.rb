6898cedc63e514064b2f6f3de002b203d9d543b6
Shaina C <scailles23@gmail.com>
2021-01-08 05:52:10 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,7 +5,7 @@ attr_accessor :date, :patient, :doctor
 @@all=[] 
 
 
-def initialize(patient, date, doctor)
+def initialize(date, patient, doctor)
   @patient = patient
   @date= date
   @doctor= doctor
